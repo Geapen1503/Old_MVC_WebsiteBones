5de62d4ea3b99c7ba23a489b495a811c25d14744
Geapen <108426543+Geapen1503@users.noreply.github.com>
2024-04-01 10:11:07 +0200

    Update p1.php

--- squeletteSite1Mvc/view/p1.php
+++ squeletteSite1Mvc/view/p1.php
@@ -23,7 +23,7 @@ ob_start();
                 </div>
                 <div class="bottomBox">
                     <p class="prestationTitle">Partenaire 1</p>
-                    <p class="prestationDescription">lorem ipsum lorem Not really haha, i'm just killing time waiting unity to load ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                    <p class="prestationDescription">lorem ipsum lorem Someone is really reading this ? ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
                 </div>
             </div>
 
